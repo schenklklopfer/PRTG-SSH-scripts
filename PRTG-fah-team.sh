525b0eaa16fac8438fc55d3a8f21be6e2d31a254
schenklklopfer <wolff@wsg-film.de>
2020-05-27 10:59:08 +0200

    Update PRTG-fah-team.sh
    added check for parameters
    added limit for 50 Users

--- PRTG-fah-team.sh
+++ PRTG-fah-team.sh
@@ -3,6 +3,11 @@
 #       Manuel Wolff - 2020
 #
 
+if [ -z $1 ] || [ -z $2 ]; then
+        echo "ERROR: No parameters given"
+        exit 1
+fi
+
 TEAM_ID=$1
 TEAM_NAME=$2
 
@@ -15,7 +20,7 @@ curl -s https://stats.foldingathome.org/api/team/$TEAM_ID > $OUT
 cat $OUT | jq . -S > $DATA
 
 echo "<prtg>"
-for MEMBER in $(cat $DATA | grep "\"name\": \"" | grep -v $TEAM_NAME | awk '{print $2}' | cut -c 2- | rev | cut -c 3- | rev | sort | uniq); do
+for MEMBER in $(cat $DATA | grep "\"name\": \"" | grep -v $TEAM_NAME | awk '{print $2}' | cut -c 2- | rev | cut -c 3- | rev | sort | uniq | head -50); do
         echo " <result>"
         echo "  <channel>$MEMBER</channel>"
         echo "  <VolumeSize>Credits</VolumeSize>"
